66542d6295b3bde28f372a4b896a68763c1d0c06
Felipe Viana <felipetiburcioviana@gmail.com>
2024-09-07 18:36:11 -0700

    Addiing Styles

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -7,32 +7,44 @@ MainFrame::MainFrame(const wxString& title) :
 
 {
 	wxPanel* panel = new wxPanel(this);
-	wxButton* button = new wxButton(panel, wxID_ANY, "Button", wxPoint(150, 50), wxSize(100, 35));
 
-	wxCheckBox* checkBox = new wxCheckBox(panel, wxID_ANY, "Check Box", wxPoint(550, 55));
+	wxButton* button = new wxButton(panel, wxID_ANY, "Button", wxPoint(150, 50), wxSize(100, 35), 
+	/* style */ wxBU_LEFT);
 
-	wxStaticText* staticText = new wxStaticText(panel, wxID_ANY, " Static Text - NOT editable", wxPoint(120, 150));
+	wxCheckBox* checkBox = new wxCheckBox(panel, wxID_ANY, "Check Box", wxPoint(550, 55), wxDefaultSize, 
+	/* style */ wxCHK_3STATE | wxCHK_ALLOW_3RD_STATE_FOR_USER);
 
-	wxTextCtrl* textCtrl = new wxTextCtrl(panel, wxID_ANY, "TextCtrl - editable", wxPoint(500, 145), wxSize(200, -1));
+	wxStaticText* staticText = new wxStaticText(panel, wxID_ANY, " Static Text - NOT editable", wxPoint(0, 150), wxSize(400, -1),
+	/* style */ wxALIGN_CENTER_HORIZONTAL );
+	staticText->SetBackgroundColour( *wxLIGHT_GREY);
+
+	wxTextCtrl* textCtrl = new wxTextCtrl(panel, wxID_ANY, "TextCtrl - editable", wxPoint(500, 145), wxSize(200, -1), 
+	/* style */ wxTE_PASSWORD);
 
 
-	wxSlider* slider = new wxSlider(panel, wxID_ANY, 25, 0, 100, wxPoint(100, 250), wxSize(200, -1));
+	wxSlider* slider = new wxSlider(panel, wxID_ANY, 25, 0, 100, wxPoint(100, 250), wxSize(200, -1),
+	/* style */ wxSL_VALUE_LABEL);
 
-	wxGauge* Gauge = new wxGauge(panel, wxID_ANY, 100, wxPoint(500, 255), wxSize(200, -1));
+	wxGauge* Gauge = new wxGauge(panel, wxID_ANY, 100, wxPoint(590, 205), wxSize(-1, 125), 
+	/* style */	wxGA_VERTICAL);
 	Gauge->SetValue(50);
 
 	wxArrayString choices;
-	choices.Add("Item A");
 	choices.Add("Item B");
 	choices.Add("Item C");
+	choices.Add("Item A");
 
-	wxChoice* choice = new wxChoice(panel, wxID_ANY, wxPoint(150, 375), wxSize(100, -1), choices);
+	wxChoice* choice = new wxChoice(panel, wxID_ANY, wxPoint(150, 375), wxSize(100, -1), choices, 
+	/* style */ wxCB_SORT);
 	choice->Select(0);
 
 
-	wxSpinCtrl* spinCtrl = new wxSpinCtrl(panel, wxID_ANY, "", wxPoint(550, 375), wxSize(100, -1));
+	wxSpinCtrl* spinCtrl = new wxSpinCtrl(panel, wxID_ANY, "", wxPoint(550, 375), wxSize(100, -1), 
+	/* style */wxSP_WRAP);
 
-	wxListBox* listBox = new wxListBox(panel, wxID_ANY, wxPoint(150, 475), wxSize(100, -1), choices);
+	wxListBox* listBox = new wxListBox(panel, wxID_ANY, wxPoint(150, 475), wxSize(100, -1), choices, 
+	/* style */	wxLB_MULTIPLE);
 
-	wxRadioBox* RadioBox = new wxRadioBox(panel, wxID_ANY, "Radio Box", wxPoint(485, 475), wxDefaultSize, choices);
+	wxRadioBox* RadioBox = new wxRadioBox(panel, wxID_ANY, "Radio Box", wxPoint(555, 450), wxDefaultSize, choices, 3,
+	/* style */ wxRA_SPECIFY_ROWS);
 }
